b56036d3e3f1d5f8c4598024d5130393401ad815
anlatbakalim24-dot <anlatbakalim24@gmail.com>
2026-04-08 04:37:40 +0300

    Update hizli.js

--- hizli.js
+++ hizli.js
@@ -75,6 +75,7 @@ function startTimer() {
         const levelElem = document.getElementById("level-display");
         if(levelElem) {
             levelElem.innerText = `Level: ${currentLevel};
+            }
             // Süreyi güncelle (timer-display'e gönderiyoruz)
         const timerElem = document.getElementById("timer-display");
         if(timerElem) {
